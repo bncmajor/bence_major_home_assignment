07612e16a88ce68efdbd2e654ae3617d31342ac8
bncmajor <major.bence.98@gmail.com>
2026-01-30 23:11:22 +0100

    minor fixes, swapping regional nat gateway to individual per AZ, adding security groups

--- exercise_1/modules/vpc/main.tf
+++ exercise_1/modules/vpc/main.tf
@@ -1,11 +1,18 @@
 resource "aws_vpc" "main" {
   region = var.region
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_vpc_endpoint" "s3" {
     vpc_id = aws_vpc.main.id
     vpc_endpoint_type = "Gateway"
     service_name = "com.amazonaws.${var.region}.s3"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  for_each = var.private_subnets
+  route_table_id  = aws_route_table.private[each.key].id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
 }
 
 resource "aws_subnet" "public" {
@@ -59,16 +66,17 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_nat_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  availability_mode = "regional"
+  for_each = var.private_subnets
+  subnet_id = aws_subnet.private[each.key].id
 }
 
 resource "aws_route_table" "private" {
+  for_each = var.private_subnets
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
+    nat_gateway_id = aws_nat_gateway.main[each.key].id
   }
 
   tags = {
@@ -79,9 +87,52 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private" {
   for_each = var.private_subnets
   subnet_id      = aws_subnet.private[each.key].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[each.key].id
 }
 
+
+resource "aws_security_group" "load_balancer_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
+resource "aws_security_group" app_server_sg {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
